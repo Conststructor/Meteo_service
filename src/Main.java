394f3abe00659a9f381596256d2459674e500fe5
Const <Const@notemail.com>
2025-11-09 19:11:51 +0500

    add some comment

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,8 @@ public class Main {
 
         MeteoService meteoService = new MeteoService();
         System.out.println("Start meteo service program!");
+
+        // meteoService.sendRequset("координаты широты", "координаты долготы", количество дней в int (от 1 до 7));
         meteoService.sendRequset("55.753883", "37.621626", 5);
 
     }
